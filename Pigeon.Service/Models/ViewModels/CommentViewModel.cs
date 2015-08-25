30f8e5ceb43cf8bdf02f48ecd976fc7544a49634
Petar Ivanov <petar.ivanov256@gmail.com>
2015-08-25 11:18:39 +0300

    Forgot to add the new files

---
+++ Pigeon.Service/Models/ViewModels/CommentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Pigeon.Models;
+
+namespace Pigeon.Service.Models.ViewModels
+{
+    public class CommentViewModel
+    {
+        public CommentViewModel()
+        {
+            
+        }
+
+        public CommentViewModel(Comment c)
+        {
+            this.Content = c.Content;
+            this.Owner = new UserViewModel()
+            {
+                Name = c.Author.FirstName + " " + c.Author.LastName,
+                Email = c.Author.Email
+            };
+        }
+
+        public string Content { get; set; }
+
+        public UserViewModel Owner { get; set; }
+    }
+}
